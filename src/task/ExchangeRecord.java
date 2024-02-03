8c1ba076d4e9cdd5366b9477d662822efcdb2149
Dennis Schönberg <dennis_schoenberg@yahoo.de>
2024-02-03 22:56:14 +0100

    new version3

--- src/task/ExchangeRecord.java
+++ src/task/ExchangeRecord.java
@@ -3,14 +3,23 @@ package task;
 import java.text.DecimalFormat;
 import java.util.Date;
 
+// Класс ExchangeRecord содержит поля для хранения информации об обмене
+// (дата, сумма, исходная и целевая валюты, результат) и метод toString(),
+// который возвращает строковое представление объекта.
+// Метод toString() используется для удобного вывода информации о записи
+// об обмене при просмотре истории обменов.
+
 public class ExchangeRecord {
-    private final Date date;
-    private final double amount;
-    private final CurrencyEnum sourceCurrency;
-    private final CurrencyEnum targetCurrency;
-    private final double resultAmount;
+    private final Date date; // Дата и время обмена
+    private final double amount; // Исходная сумма для обмена
+    private final CurrencyEnum sourceCurrency; // Исходная валюта
+    private final CurrencyEnum targetCurrency; // Целевая валюта
+    private final double resultAmount; // Результат обмена
+
+    // Формат для отображения чисел
     private final DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
+    // Конструктор для создания записи об обмене
     public ExchangeRecord(Date date, double amount, CurrencyEnum sourceCurrency, CurrencyEnum targetCurrency, double resultAmount) {
         this.date = date;
         this.amount = amount;
@@ -18,6 +27,8 @@ public class ExchangeRecord {
         this.targetCurrency = targetCurrency;
         this.resultAmount = resultAmount;
     }
+
+    // Переопределение метода toString для удобного вывода информации о записи об обмене
 
     @Override
     public String toString() {
